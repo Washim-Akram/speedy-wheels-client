abfc138992ad2a1a76007fc795f12ce53adec284
Washim-Akram <muhammadwashimakram@gmail.com>
2023-05-19 01:15:12 +0600

    Added homepage banner section

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,7 +1,46 @@
 const Banner = () => {
   return (
-    <div>
-      <h2>This is Banner</h2>
+    <div className="mt-20 container mx-auto">
+      <div className="carousel w-full">
+        <div id="item1" className="carousel-item w-full">
+          <img
+            src="https://images.pexels.com/photos/2527931/pexels-photo-2527931.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            className="w-full h-[600px]"
+          />
+        </div>
+        <div id="item2" className="carousel-item w-full">
+          <img
+            src="https://images.pexels.com/photos/574519/pexels-photo-574519.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            className="w-full h-[600px]"
+          />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img
+            src="https://images.pexels.com/photos/97353/pexels-photo-97353.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            className="w-full h-[600px]"
+          />
+        </div>
+        <div id="item4" className="carousel-item w-full">
+          <img
+            src="https://images.pexels.com/photos/92615/pexels-photo-92615.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            className="w-full h-[600px]"
+          />
+        </div>
+      </div>
+      <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" className="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs">
+          3
+        </a>
+        <a href="#item4" className="btn btn-xs">
+          4
+        </a>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,7 +3,6 @@ import Banner from "../Banner/Banner";
 const Home = () => {
   return (
     <>
-      <h2>This is Home</h2>
       <Banner></Banner>
     </>
   );
