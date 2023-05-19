2097de0e243fafbc2afc02f6c527c1d650ec5785
Washim-Akram <muhammadwashimakram@gmail.com>
2023-05-19 16:13:54 +0600

    Added Blogs page and created layout for it

---
+++ src/pages/Blogs/Blogs.jsx
@@ -0,0 +1,98 @@
+const Blogs = () => {
+  return (
+    <div className="container mx-auto mt-16 mb-24">
+      <h2 className="text-center text-5xl mb-12 underline">
+        Welcome to Our Blogs!
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 bg-base-200 p-10 md:p-16 lg:p-20 rounded-lg">
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">
+              What is an access token and refresh token? How do they work and
+              where should we store them on the client-side?
+            </h2>
+            <div className="divider"></div>
+            <p className="text-justify font-light" style={{ fontSize: "18px" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              harum exercitationem quasi et obcaecati eos ullam quaerat fuga quo
+              quos. Veritatis voluptatibus laborum qui inventore est eos,
+              quibusdam ex sapiente nam at neque, enim itaque odit architecto.
+              Ab, eveniet aliquid laborum neque, quod quo eos, dolorem
+              distinctio beatae doloremque itaque explicabo quaerat at doloribus
+              magnam sapiente eaque velit eum! Explicabo illum in numquam
+              architecto quasi laudantium, nisi consequatur ullam fugit eum
+              quidem quod animi laboriosam adipisci eveniet tempora. Minima unde
+              quia optio iusto reiciendis itaque tempore facere, aliquam
+              repudiandae vero veritatis mollitia magni aut distinctio, eius est
+              dolor reprehenderit! Nihil.
+            </p>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Compare SQL and NoSQL databases?</h2>
+            <div className="divider"></div>
+            <p className="text-justify font-light" style={{ fontSize: "18px" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              harum exercitationem quasi et obcaecati eos ullam quaerat fuga quo
+              quos. Veritatis voluptatibus laborum qui inventore est eos,
+              quibusdam ex sapiente nam at neque, enim itaque odit architecto.
+              Ab, eveniet aliquid laborum neque, quod quo eos, dolorem
+              distinctio beatae doloremque itaque explicabo quaerat at doloribus
+              magnam sapiente eaque velit eum! Explicabo illum in numquam
+              architecto quasi laudantium, nisi consequatur ullam fugit eum
+              quidem quod animi laboriosam adipisci eveniet tempora. Minima unde
+              quia optio iusto reiciendis itaque tempore facere, aliquam
+              repudiandae vero veritatis mollitia magni aut distinctio, eius est
+              dolor reprehenderit! Nihil.
+            </p>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">What is express js? What is Nest JS?</h2>
+            <div className="divider"></div>
+            <p className="text-justify font-light" style={{ fontSize: "18px" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              harum exercitationem quasi et obcaecati eos ullam quaerat fuga quo
+              quos. Veritatis voluptatibus laborum qui inventore est eos,
+              quibusdam ex sapiente nam at neque, enim itaque odit architecto.
+              Ab, eveniet aliquid laborum neque, quod quo eos, dolorem
+              distinctio beatae doloremque itaque explicabo quaerat at doloribus
+              magnam sapiente eaque velit eum! Explicabo illum in numquam
+              architecto quasi laudantium, nisi consequatur ullam fugit eum
+              quidem quod animi laboriosam adipisci eveniet tempora. Minima unde
+              quia optio iusto reiciendis itaque tempore facere, aliquam
+              repudiandae vero veritatis mollitia magni aut distinctio, eius est
+              dolor reprehenderit! Nihil.
+            </p>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">
+              What is MongoDB aggregate and how does it work?
+            </h2>
+            <div className="divider"></div>
+            <p className="text-justify font-light" style={{ fontSize: "18px" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              harum exercitationem quasi et obcaecati eos ullam quaerat fuga quo
+              quos. Veritatis voluptatibus laborum qui inventore est eos,
+              quibusdam ex sapiente nam at neque, enim itaque odit architecto.
+              Ab, eveniet aliquid laborum neque, quod quo eos, dolorem
+              distinctio beatae doloremque itaque explicabo quaerat at doloribus
+              magnam sapiente eaque velit eum! Explicabo illum in numquam
+              architecto quasi laudantium, nisi consequatur ullam fugit eum
+              quidem quod animi laboriosam adipisci eveniet tempora. Minima unde
+              quia optio iusto reiciendis itaque tempore facere, aliquam
+              repudiandae vero veritatis mollitia magni aut distinctio, eius est
+              dolor reprehenderit! Nihil.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
